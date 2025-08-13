f37264eac8b9adf2fd70a4d03366964f11f72d05
VioletDoingGarden <phamtri866@gmail.com>
2025-08-13 19:22:13 +0930

    pr3

--- Reverser.cpp
+++ Reverser.cpp
@@ -3,6 +3,7 @@
 #include "Reverser.h"
 
 int Reverser::reverseDigit(int value){
+    if(value < 0) return -1; 
     int size = 0;
     for(int temp = value; temp > 0; temp /= 10) {
         size++;
@@ -12,6 +13,7 @@ int Reverser::reverseDigit(int value){
     return reverseDigit(value / 10)+ (value % 10) * pow(10,size - 1);
 }
 std::string Reverser::reverseString(std::string characters){
+    if(characters.empty()) return "ERROR";
     if(characters.length() <= 1) return characters;
     return reverseString(characters.substr(1)) + characters[0];
 }
