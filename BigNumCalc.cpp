5dd35cea6acd078b1f999e43b640f9ad2731eadb
VioletDoingGarden <phamtri866@gmail.com>
2025-09-14 22:12:51 +0930

    pr6

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -53,25 +53,33 @@ std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
     auto it1 = num1.begin();
     auto it2 = num2.end();
     
-    while (it1 != num1.end()) {
-        int diff = *it1 - borrow;
-        if (it2 != num2.end()) {
-            diff -= *it2;
-            ++it2;
+    while (it1 != num1.end()){
+        int digit2 = *it2; 
+        if (it2 != num2.end()){
+            it2++; 
+        }else{
+            digit2 = 0;
         }
-        if (diff < 0) {
-            diff += 10;
-            borrow = 1;
-        } 
+
+        int digit1 = *it1 - borrow;
+        
+
+        if (digit2 > digit1){           
+            digit1 = digit1 + 10;       
+            borrow = 1;                  
+        } else {
+            borrow = 0;                 
+        }
+        int diff = digit1 - digit2;
         res.push_front(diff);
-        ++it1;
+        it1++;
     }
-    
-    while (res.size() > 1 && res.front() == 0) {
+    while (res.size() > 1 && res.front() == 0)
+    {
         res.pop_front();
     }
-    
     return res;
+
 }
 
 std::list<int> BigNumCalc::mul(std::list<int> num1, std::list<int> num2) {
