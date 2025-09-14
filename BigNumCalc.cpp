80aebebfda056026b4e7dcedc683a2b5f416689e
VioletDoingGarden <phamtri866@gmail.com>
2025-09-14 22:07:20 +0930

    pr6

--- BigNumCalc.cpp
+++ BigNumCalc.cpp
@@ -45,27 +45,24 @@ std::list<int> BigNumCalc::add(std::list<int> num1, std::list<int> num2) {
 
 std::list<int> BigNumCalc::sub(std::list<int> num1, std::list<int> num2) {
     std::list<int> res;
-    int carry = 0;
+    int borrow = 0;
     
-    // num1.reverse();
-    // num2.reverse();
+    num1.reverse();
+    num2.reverse();
 
-    auto it1 = num1.rbegin();
-    auto it2 = num2.rend();
+    auto it1 = num1.begin();
+    auto it2 = num2.end();
     
-    while (it1 != num1.rend()) {
-        int d1 = *it2;
-        if(it2 != num2.rend()) it2++;
-        else d1 = 0;
-        int d2 =  *it1 - carry;
-        if (d2 > d1)
-        {
-            d1 = d1 + 10;  
-            carry = 1;
-
-        }else   carry = 0;
-        int diff = d1 - d2; 
-        
+    while (it1 != num1.end()) {
+        int diff = *it1 - borrow;
+        if (it2 != num2.end()) {
+            diff -= *it2;
+            ++it2;
+        }
+        if (diff < 0) {
+            diff += 10;
+            borrow = 1;
+        } 
         res.push_front(diff);
         ++it1;
     }
